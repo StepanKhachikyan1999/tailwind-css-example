9b3ef6985cc68b7a7a61adef3b325401339c5eec
Stepan <stepan.khachikyan@beeoncode.com>
2022-01-24 17:35:47 +0400

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+// for(let num = 5; num > 0;num--) {
+//     let str = ""
+
+//     for(let i = 0;i < num; i++) {
+//         str = str + "*"
+//     }
+//     console.log(str);
+// }
+
+let num = 5
+
+for(i = 1;i <= num;i++) {
+    let str = ""
+
+    for(let j = 0; j < i;j++) {
+        str = str + i
+    }
+
+    console.log(str);
+}
